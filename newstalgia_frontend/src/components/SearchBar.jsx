132e8668e75b0a9f2749b8f4f3d735593f95125a
markestiller <markalestiller@gmail.com>
2024-01-27 14:59:23 -0500

    created search bar

---
+++ newstalgia_frontend/src/components/SearchBar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const SearchBar = () => {
+  const [input, setInput] = useState("");
+  const [searchResult, setSearchResult] = useState("");
+
+  const handleSearch = () => {
+    console.log(`Searching for: ${input}`);
+
+    // todo: fill up endpoint, store data (search result) in useState
+    const searchEndpoint = "";
+
+    fetch(searchEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(input))
+      .catch((error) => console.log(error));
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key == "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="flex max-w-md mx-auto p-4 ">
+      <input
+        className="flex w-full border p-2 rounded-md text-xl"
+        type="text"
+        placeholder="Search for new news..."
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+        onKeyDown={handleKeyPress}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
