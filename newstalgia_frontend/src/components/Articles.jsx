a533b37f9026036cffdd91b81e24cb325c495b18
jessie wang <117032848+jwaanng@users.noreply.github.com>
2024-01-27 16:23:37 -0500

    article component done + articles component wip + Title

---
+++ newstalgia_frontend/src/components/Articles.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./NewsArticle.css";
+import PropTypes from 'prop-types';
+import NewsArticle from "./newsArticle";
+
+const Articles = () => {
+
+  return (
+    <div className="articles">
+        <NewsArticle heading="shoot not the stars" authors="mark" content="skdjfkls sdlfjls jdf lj lsjdfskld sfdj sdj lksd klsj lsdklfj lsdkfskldfj ljsdlj ksldfjkljsdklfj kjdflj sfdlj lsdjf slkfjl jsdf jklsdjfjf sdljlsdkj dlkfjdsl jldjfldskjflskdj lsdjflkj sdlkjf lsjdkfjlsd lsdfj lksjflkjdlks jsdlkfj sldjf lsdkj lkdsjfl jsdl dslkfjdlj"/>
+        <NewsArticle heading="shoot not the stars" authors="mark" content="skdjfkls sdlfjls jdf lj lsjdfskld sfdj sdj lksd klsj lsdklfj lsdkfskldfj ljsdlj ksldfjkljsdklfj kjdflj sfdlj lsdjf slkfjl jsdf jklsdjfjf sdljlsdkj dlkfjdsl jldjfldskjflskdj lsdjflkj sdlkjf lsjdkfjlsd lsdfj lksjflkjdlks jsdlkfj sldjf lsdkj lkdsjfl jsdl dslkfjdlj"/>
+
+    </div>
+  );
+}
+
+export default Articles;
---
+++ newstalgia_frontend/src/components/NewstalgiaTitle.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./NewstalgiaTitle.css";
+
+const NewstalgiaTitle = () => {
+  return (
+    <>
+        <div></div>
+        <h1 className="newstalgia-title">Newstalgia</h1>
+        <div></div>
+    </>
+  );
+};
+
+export default NewstalgiaTitle;
