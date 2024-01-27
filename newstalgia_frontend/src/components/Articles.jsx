c95da9bb67f99e787ba98be641f37b1bee691b91
markestiller <markalestiller@gmail.com>
2024-01-27 17:50:26 -0500

    modified Articles to call backend endpoint

--- newstalgia_frontend/src/components/Articles.jsx
+++ newstalgia_frontend/src/components/Articles.jsx
@@ -1,16 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Articles.css";
 import NewsArticle from "./newsArticle";
 
 const Articles = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch("http:localhost3000/news/home", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles(
+          data["articles"].map((article) => ({
+            heading: article.title,
+            authors: article.author,
+            content: article.content,
+          }))
+        );
+      })
+      .catch((error) => console.log(error));
+  });
+
+  // dummy data
+  // const articles = [
+  //   {
+  //     heading: "headingone",
+  //     author: "authorone",
+  //     content: "contentone",
+  //   },
+  //   {
+  //     heading: "headingone",
+  //     author: "authorone",
+  //     content: "contentone",
+  //   },
+  //   {
+  //     heading: "headingone",
+  //     author: "authorone",
+  //     content: "contentone",
+  //   },
+  // ];
 
   return (
     <div className="articles">
-        <NewsArticle heading="shoot not the stars" authors="mark" content="skdjfkls sdlfjls jdf lj lsjdfskld sfdj sdj lksd klsj lsdklfj lsdkfskldfj ljsdlj ksldfjkljsdklfj kjdflj sfdlj lsdjf slkfjl jsdf jklsdjfjf sdljlsdkj dlkfjdsl jldjfldskjflskdj lsdjflkj sdlkjf lsjdkfjlsd lsdfj lksjflkjdlks jsdlkfj sldjf lsdkj lkdsjfl jsdl dslkfjdlj"/>
-        <NewsArticle heading="shoot not the stars" authors="mark" content="skdjfkls sdlfjls jdf lj lsjdfskld sfdj sdj lksd klsj lsdklfj lsdkfskldfj ljsdlj ksldfjkljsdklfj kjdflj sfdlj lsdjf slkfjl jsdf jklsdjfjf sdljlsdkj dlkfjdsl jldjfldskjflskdj lsdjflkj sdlkjf lsjdkfjlsd lsdfj lksjflkjdlks jsdlkfj sldjf lsdkj lkdsjfl jsdl dslkfjdlj"/>
-
+      {articles.map((article, index) => (
+        <li key={index}>
+          <NewsArticle
+            heading={article.heading}
+            authors={article.author}
+            content={article.content}
+          />
+        </li>
+      ))}
     </div>
   );
-}
+};
 
 export default Articles;
