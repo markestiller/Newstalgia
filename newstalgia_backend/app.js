f71c4ab8ce1ae72cbe845ce4b24b729ae28f893d
markestiller <markalestiller@gmail.com>
2024-01-27 12:55:15 -0500

    initial backend setup

--- newstalgia_backend/app.js
+++ newstalgia_backend/app.js
@@ -1,1 +1,8 @@
+require("dotenv").config(); // Load environment variables
+
 const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
