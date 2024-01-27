d519bba40ed1894c13939c4bf85916d5c7684763
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-01-27 13:38:40 -0500

    Setup backend

--- newstalgia_backend/app.js
+++ newstalgia_backend/app.js
@@ -1,8 +1,30 @@
-require("dotenv").config(); // Load environment variables
-
+const createError = require("http-errors");
 const express = require("express");
-const fetch = require("node-fetch");
-const app = express();
-const port = 3000;
+const logger = require("morgan");
+const cors = require("cors");
 
+const indexRouter = require("./routes/index");
+
+const errorHandler = require("./error-handler");
+
+const app = express();
+
+// Enable cors
+app.use(cors());
+app.options("*", cors());
+
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(errorHandler.errorHandlerMiddleware);
+
+module.exports = app;
