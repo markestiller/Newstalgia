96411c6deb67029f61f608234e49ea2515523b5d
jessie wang <117032848+jwaanng@users.noreply.github.com>
2024-01-27 16:27:26 -0500

    app

--- newstalgia_frontend/src/components/newsArticle.jsx
+++ newstalgia_frontend/src/components/newsArticle.jsx
@@ -2,21 +2,26 @@ import { useState } from "react";
 import "./NewsArticle.css";
 import PropTypes from 'prop-types';
 
-const NewsArticle = ({ heading, author, text, image }) => {
+const NewsArticle = ({ heading, authors, content, image }) => {
+    // function that capitalizes
+    const capitalizeString = (str) => {
+        return str.toUpperCase();
+    };
+
+
   return (
     <div className="news-article">
-        <h2>{heading}</h2>
-        <img src={image} alt="Article" />
-        <p><strong>Author:</strong> {author}</p>
-        <p>{text}</p>
+        <h2 className="heading"><b>{capitalizeString(heading)}</b></h2>
+        <p className="authors"><b>{authors}</b></p>
+        <p className="content">{content}</p>
     </div>
   );
 }
 
 NewsArticle.propTypes = {
   heading: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  authors: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired
 };
 
