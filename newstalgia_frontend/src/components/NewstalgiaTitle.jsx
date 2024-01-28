69e93027723ba263db5834285b072fad9029bd59
jessie wang <117032848+jwaanng@users.noreply.github.com>
2024-01-28 04:31:50 -0500

    better dummy data

--- newstalgia_frontend/src/components/NewstalgiaTitle.jsx
+++ newstalgia_frontend/src/components/NewstalgiaTitle.jsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./NewstalgiaTitle.css";
 
 const NewstalgiaTitle = () => {
+  
+  // const [quote, setQuote] = useState('');
+
+  // useEffect(() => {
+    
+  //   const proxyUrl = 'https://cors-anywhere.herokuapp.com/corsdemo';
+  //   const apiUrl = 'https://zenquotes.io/api/today';
+    
+  //   const fetchQuote = async () => {
+  //     try {
+  //       const response = await fetch(proxyUrl + apiUrl);
+  //       const data = await response.json();
+  //       console.log(data);
+  //       const quoteText = data[0].q;
+  //       setQuote(quoteText);
+  //     } catch (error) {
+  //       console.error('Error fetching quote:', error);
+  //     }
+  //   };
+
+  //   fetchQuote();
+  // }, []);
+
   return (
     <>
-        <div></div>
+        <div>
+          {/* <h2><blockquote>{quote}</blockquote></h2> */}
+        </div>
         <h1 className="newstalgia-title">Newstalgia</h1>
         <div></div>
     </>
